c4bf0ab2cc249a228264f2cd0b6d665ba40b384a
Ahikahi <ahmetkarabag366@gmail.com>
2025-10-15 21:17:12 +0200

    alle_forms_Aufgaben_erledigt

--- calculations/forms/hypotenuse.go
+++ calculations/forms/hypotenuse.go
@@ -1,8 +1,9 @@
 package forms
+import "math"
 
 // Erwartet die Längen der Katheten eines rechtwinkligen Dreiecks.
 // Liefert die Länge der Hypotenuse.
 func Hypotenuse(a, b float64) float64 {
-	// TODO
-	return 0
+	c := math.Hypot(a , b)
+	return c
 }
--- calculations/forms/perimeter_right_triangle.go
+++ calculations/forms/perimeter_right_triangle.go
@@ -1,8 +1,12 @@
 package forms
 
+import "math"
+
 // Erwartet zwei Seitenlängen a und b.
 // Liefert den Umfang eines rechtwinkligen Dreiecks, dessen Katheten a und b sind.
 func PerimeterRightTriangle(a, b float64) float64 {
 	// TODO
-	return 0
+	c := math.Sqrt(a*a+b*b)
+	seitenrechnen := a + b +c
+	return seitenrechnen
 }
